b56b9503c66ec3a8fe9d8f40f48524d51563e1f5
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2021-02-22 18:39:20 +0900

    1.3. Вызов метода с аргументами. [#341349]

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -2,14 +2,18 @@ package ru.job4j.oop;
 
 public class Jukebox {
     public void music(int position) {
-        if (position == 1)
-            System.out.println("Пусть бегут неуклюже");
 
-        if (position == 2)
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже");
+        }
+
+
+        if (position == 2) {
             System.out.println("Спокойной ночи");
-        else
+        } else {
 
             System.out.println("Песня не найдена");
+        }
     }
 
 
