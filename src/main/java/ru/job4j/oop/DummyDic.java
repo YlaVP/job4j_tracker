e585c20c0c2c5af9be9c13dd4a5a0429d93aa32e
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2021-02-23 11:55:15 +0900

    Исправления

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -2,16 +2,13 @@ package ru.job4j.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-        return eng;
+        return ("Неизвестное слово. " + eng);
 
     }
 
     public static void main(String[] args) {
         DummyDic dummyDic = new DummyDic();
         String eng = dummyDic.engToRus("latter");
-
-
-        System.out.println("Неизвестное слово. " + eng);
-
+        System.out.println(eng);
     }
 }
