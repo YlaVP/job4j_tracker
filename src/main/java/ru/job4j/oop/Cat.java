ea029943a2f6ad477fbcb230dd65d4171061ab1a
YlaVP <git@github.com:YlaVP/job4j_elementary.git>
2021-02-23 20:51:55 +0900

    1.5. Поля объекта [#341347]1

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,26 @@
+package ru.job4j.oop;
+
+public class Cat {
+    private String name;
+    private String food;
+
+
+    public void giveNick(String nick) {
+        this.name = nick;
+
+    }
+
+    public void show() {
+        System.out.println("Cat is name " + name + ". " + " Cat is eating a " + food);
+
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.giveNick("Fofa");
+        cat.food = "сhicken";
+        cat.show();
+
+    }
+
+}
