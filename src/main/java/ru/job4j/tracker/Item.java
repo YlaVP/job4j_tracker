a5a44192c5ed600014f47c4604453ee6a7e5375e
YlaVP <git@github.com:YlaVP/restart.git>
2021-07-07 19:38:53 +0900

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Item {
 
@@ -42,6 +43,27 @@ public class Item {
         return created;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void add(Item item) {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id && Objects.equals(name, item.name) && Objects.equals(created, item.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, created);
+    }
+
 
 
 }
+
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,20 +1,35 @@
 package ru.job4j.tracker;
 
 public class Tracker {
-    private final Item[] item = new Item[100];
-    private int id = 1;
+    private final Item[] items = new Item[100];
+    private int ids = 1;
     private int size = 0;
 
     public Item add(Item item) {
-        item.setId(id++);
-        this.item[size++] = item;
+        item.setId(ids++);
+        items[size++] = item;
         return item;
     }
+    public Item[] findAll() {
+        return items;
+    }
 
+    public Item[] findByName(String key) {
+        Item[] rsl = items;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+             return rsl;
+
+            }
+        }
+        return rsl;
+
+    }
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
-            Item item = this.item[index];
+            Item item = items[index];
             if (item.getId() == id) {
                 rsl = item;
                 break;
@@ -23,3 +38,5 @@ public class Tracker {
         return rsl;
     }
 }
+
+
